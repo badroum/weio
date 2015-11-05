cd23936dcb27783ddd6b9bc0fa949f828dd05ae6
badroum <julien.dufee@gmail.com>
2015-11-05 21:29:14 +0100

    test 2 dfu

--- openWrt/pkg_install.sh
+++ openWrt/pkg_install.sh
@@ -14,4 +14,4 @@
 ./scripts/feeds install python-pip
 ./scripts/feeds install avrdude
 #./scripts/feeds install nginx
-./scripts/feeds install dfu
+./scripts/feeds install https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/packages/oldpackages/dfu-util_r3095-1_ar71xx.ipk
