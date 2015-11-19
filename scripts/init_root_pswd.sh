e7d4191848647c29e6ca5edfe52886e0f582bc6c
badroum <julien.dufee@gmail.com>
2015-11-19 11:45:09 +0100

    test pass

--- scripts/init_root_pswd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "setting root password"
-echo -e "houat\nhouat" | passwd
-echo "setting weio user SMB password. Same as root password"
-(echo "houat"; echo "houat") | smbpasswd -s -a weio
--- scripts/run_weio.sh
+++ scripts/run_weio.sh
@@ -1,53 +1,4 @@
 #!/bin/sh
-### 
-#
-# WEIO Web Of Things Platform
-# Copyright (C) 2013 Nodesign.net, Uros PETREVSKI, Drasko DRASKOVIC
-# All rights reserved
-#
-#               ##      ## ######## ####  #######  
-#               ##  ##  ## ##        ##  ##     ## 
-#               ##  ##  ## ##        ##  ##     ## 
-#               ##  ##  ## ######    ##  ##     ## 
-#               ##  ##  ## ##        ##  ##     ## 
-#               ##  ##  ## ##        ##  ##     ## 
-#                ###  ###  ######## ####  #######
-#
-#                    Web Of Things Platform 
-#
-# This file is part of WEIO and is published under BSD license.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#    This product includes software developed by the WeIO project.
-# 4. Neither the name of the WeIO nor the
-# names of its contributors may be used to endorse or promote products
-# derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY WEIO PROJECT AUTHORS AND CONTRIBUTORS ''AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL WEIO PROJECT AUTHORS AND CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Authors : 
-# Uros PETREVSKI <uros@nodesign.net>
-# Drasko DRASKOVIC <drasko.draskovic@gmail.com>
-#
-###
-
 # OpenWRT patch python to disable the byte compilation by default
 # https://github.com/openwrt/packages/pull/541
 # This change is annoying as WeIO is mainly composed of python code.
@@ -67,7 +18,7 @@ check_wifi()
 {
     DELAY=5
     COUNTER=0
-        
+
     # Wait for WiFi interface to appear
     while [ ! -e /sys/class/net/wlan0/operstate ]; do
         # WiFi is still down. Wait a bit.
@@ -78,7 +29,7 @@ check_wifi()
            return
         fi
     done
-    
+
     # Reset counter
     COUNTER=0
     while [  $COUNTER -lt $DELAY ]; do
@@ -163,7 +114,7 @@ echo "===> STARTING THE SERVER"
 
 ### WE HAVE EXITED SERVER - CHECK OUT WHY ###
 echo "===> EXITED SERVER"
-    
+
 if grep -q '"kill_flag": "YES"' /weio/config.weio
 then
     echo "Found kill flag"
